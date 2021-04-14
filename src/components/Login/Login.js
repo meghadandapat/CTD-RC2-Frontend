e6579d557a285b8991e81da8d2082b7137b8b829
Varada Kalaskar <kalaskar.varada@gmail.com>
2021-04-15 00:43:15 +0530

    login first commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,35 @@
+import ReactDOM from 'react-dom';
+import  './Login.css';
+ const Login =() =>
+{
+    return (
+        <section>
+            <br></br>
+            <form>
+                <table border="1" align="center" width="30%" height="120%">
+                    <h2>Welcome!</h2>
+                Username:  <input type="text" name="Username" placeholder="USERNAME" required></input><br></br><br></br>
+                Password:  <input type="password" name="Username" placeholder="PASSWORD" required></input><br></br><br></br>
+                Email:     <input type="text" name="Username" placeholder="EMAILID" required></input><br></br><br></br> 
 
-import React from 'react';
+                 <b>-Select Category-</b><br></br><br></br>
+                <input type="radio" name="Category"/>Junior &nbsp;&nbsp;
+            <input type="radio" name="Category"/>Senior
+            <br></br>
+            <br></br>
+             <button style={{padding:'0.5rem', height:'5vh'}}>
+                 <a href="#" role="button"><b>Submit</b></a>
+                 </button>
+                 </table>
+                 </form>
 
-const Login = () => {
-    return ( <h1>This is Login page</h1> );
+                 <br></br><br></br>
+                 <footer className="footer">
+                 <span className="footer__date">
+                  {`© PICT IEEE STUDENT BRANCH ${new Date().getFullYear()}`}
+              </span>
+               </footer>
+        </section>
+    )
 }
- 
 export default Login;
