2ecb8bfd68a58ac9f4200b9158e9995918920f01
Varada Kalaskar <kalaskar.varada@gmail.com>
2021-04-14 16:29:01 +0530

    QuestionHub first commit

--- src/components/QuestionHub/QuestionHub.js
+++ src/components/QuestionHub/QuestionHub.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import './QuestionHub.css';
+import { ProgressBar, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Cards from "./Cards";
+
 
 const QuestionHub = () => {
-    return ( <h1>This is QuestionHub page</h1> );
+    const qs=[1,2,3,4,5,6];
+   
+    return (  
+        <div className="qhub">
+            <Cards qno={"Question No."} progbar={"Progress"} attempts={"Attempts"}/>
+            {qs.map((qs)=>(
+                <Cards qno={qs} progbar={<ProgressBar variant="secondary" now={Math.floor(Math.random() * (100 - 0))} /> } 
+                attempts={Math.floor(Math.random() * 10) + 1} button="yes"/>
+            ))}
+        </div>
+    );
 }
- 
+
 export default QuestionHub;
