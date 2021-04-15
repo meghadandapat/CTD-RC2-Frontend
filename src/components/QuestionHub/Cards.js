dc0cd921ed3bdb7e0e5da3a81dbd8112e6690d97
Varada Kalaskar <kalaskar.varada@gmail.com>
2021-04-15 20:32:34 +0530

    Question hub updated with colour scheme

--- src/components/QuestionHub/Cards.js
+++ src/components/QuestionHub/Cards.js
@@ -1,36 +1,44 @@
-const Cards = ({ qno, progbar, attempts, button }) => {
-  return (
-    <div className="Cards">
-      <div className="row">
-        <div className="col-sm-1"></div>
-        <div className="col-sm-10">
-          <div className="card">
-            <div className="card-body">
-              <div className="row">
-                <div className="col-sm-1"></div>
-                <div className="col-sm-2 justify-content-center">
-                  <p>{qno}</p>
-                </div>
-                <div className="col-sm-4 justify-content-center">
-                  <p>{progbar}</p>
-                </div>
-                <div className="col-sm-2 justify-content-center">
-                  <p>{attempts}</p>
-                </div>
-                <div className="col-sm-2 justify-content-right">
-                  {button === "yes" && (
-                    <div className="btn btn-dark">Attempt</div>
-                  )}
-                </div>
-                <div className="col-sm-1"></div>
+const Cards = ({qno, progbar, attempts, button}) => {
+  return ( 
+      <div className="Cards">
+          <div className="row">
+              <div className="col-sm-1">
+
               </div>
-            </div>
+              <div className="col-sm-10">
+                  <div className="card qhubcard">
+                      <div className="card-body">
+                          <div className="row">
+                              <div className="col-sm-1">
+
+                              </div>
+                              <div className="col-sm-2 justify-content-center">
+                                  <h6>{qno}</h6>
+                              </div>
+                              <div className="col-sm-4 justify-content-center">
+                                  <h6>{progbar}</h6>
+                              </div>
+                              <div className="col-sm-2 justify-content-center">
+                                  <h6>{attempts}</h6>
+                              </div>
+                              <div className="col-sm-2 justify-content-right">
+                                  {button==="yes" && <div className="btn btn-dark"><div class="attempt">Attempt</div></div>}
+                                  
+                              </div>
+                              <div className="col-sm-1">
+                                  
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div className="col-sm-1">
+
+              </div>
           </div>
-        </div>
-        <div className="col-sm-1"></div>
       </div>
-    </div>
-  );
-};
+
+   );
+}
 
 export default Cards;
