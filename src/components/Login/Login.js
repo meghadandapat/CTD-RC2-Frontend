fdac801f9b9c797288eab7efdd614f3bf266c6b3
Vaidehizade <vaidehizade2001@gmail.com>
2021-04-20 22:27:40 +0530

    login page demo

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import  './Login.css';
- const Login =() =>
-{
-    return (
-        <section>
-            <br></br>
-            <form>
-                <table border="1" align="center" width="30%" height="120%">
-                    <h2>Welcome!</h2>
-                Username:  <input type="text" name="Username" placeholder="USERNAME" required></input><br></br><br></br>
-                Password:  <input type="password" name="Username" placeholder="PASSWORD" required></input><br></br><br></br>
-                Email:     <input type="text" name="Username" placeholder="EMAILID" required></input><br></br><br></br> 
+import './Login.css';
+import User from './User';
 
-                 <b>-Select Category-</b><br></br><br></br>
-                <input type="radio" name="Category"/>Junior &nbsp;&nbsp;
-            <input type="radio" name="Category"/>Senior
-            <br></br>
-            <br></br>
-             <button style={{padding:'0.5rem', height:'5vh'}}>
-                 Submit
-                 </button>
-                 </table>
-                 </form>
+import illusion from '../assets/illusion.jpg';
 
-                 <br></br><br></br>
-                 
-        </section>
-    )
+ const Login = () =>{
+
+       return(
+      <React.Fragment>
+        <div class= "container">
+         <div class="row dbox">
+           <div class="col-sm-6 cbox">
+              <div className="card illustration">
+              <div className="card-body">
+             
+             <img src={illusion} className="illusion-container"></img>
+             </div>
+             </div></div>
+           <div class="col-sm-6 cbox">
+           <User/>
+           </div>
+           </div>
+         </div>
+      </React.Fragment>
+     )
 }
 export default Login;
---
+++ src/components/Login/User.js
@@ -0,0 +1,63 @@
+import React ,{useState}from 'react';
+import Radio from '@material-ui/core/Radio';
+
+const User = () => {
+    const[name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <div className="login"> 
+        <div className="card login-card">
+              <div className="card-header mid">
+                <h1>Welcome!</h1></div>
+                 <div className="card-body">
+                         <form action="" className="main">
+        
+                                 <div class="form-group row head">
+                        
+                                   <i class='fas fa-user-circle rid'></i>
+                      
+                        <input type="required" class="form-control" name="name" id="name" placeholder= "Username" value={name} onChange = {
+                        (e) => setName(e.target.value)
+                    }></input>
+                    </div>
+                <div>
+                     <div class="form-group row head1">
+                         
+                    <label htmlFor="email"></label>
+                    <i class='fas fa-envelope-open rid'></i>
+                    <input  type="text" name="email"  class="form-control" id="email" placeholder= "Email" value={email} onChange = {
+                        (e) => setEmail(e.target.value)}></input>
+                        </div>
+                    </div>
+             
+
+                <div>
+                     <div class="form-group row head2">
+                          
+                         <i class='fas fa-lock rid'></i>
+                         
+                    <label htmlFor="password"></label> 
+                    <input type="password" name="password" class="form-control" id="password" placeholder= "Password"  autoComplete="off" value={password} 
+                    onChange = {
+                        (e) => setPassword(e.target.value)}></input>
+                        </div>
+                        </div>
+              
+                <div class="form-grp">
+                    <Radio/>Junior  <Radio/>Senior
+                   <br/>
+                   <button className="wid" type="btn" alt="button">Login</button>
+                  </div>
+                  
+                 
+            </form>
+        </div> 
+        </div>
+        </div>
+
+    )
+}
+
+export default User
