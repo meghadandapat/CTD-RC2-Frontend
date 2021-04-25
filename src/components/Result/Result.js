9b971e1a0cc94165e34d18f158bb379ef7d71d29
eshanvekar-01 <eshaanvekar@gmail.com>
2021-04-25 23:17:30 +0530

    Results modified

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -9,20 +9,35 @@ import bronze from '../svgs/Bronze.svg'
 
 const Result = () => {
     return ( 
-        <div className="fluid-container resultpage">
+        <div className="fluid-container resultpage ">
              
             <div className="row" style={{marginTop:"7vh"}}>
-                <div className="col-sm-4">
+                <div className="col-sm-4 ">
                     <div className="row"><Resultcards header="Username" result="ABC"/></div>
                     <div className="row"><Resultcards header="Rank" result="1"/></div>
                     <div className="row"><Resultcards header="Score" result="80"/></div>
                 </div>
                 <div className="col-sm-8">
-                    <div className="row">
-                    <img src ={gold} style={{width:"100px",height:"100px"}} alt="My Happy SVG"/><span>1. Megha Dandapat</span></div>
-                    <div className="row">
-                    <img src ={silver} style={{width:"100px",height:"100px"}} alt="My Happy SVG"/>2. Varada kalaskar</div>
-                    <div className="row"> <img src ={bronze} style={{width:"100px",height:"100px"}} alt="My Happy SVG"/>3. Esha Anvekar</div>
+                    <div className="row ">
+                        <div className="col">
+                        <img className="svgs silver svg-down" src ={silver} alt="My Happy SVG"/>
+                        <p className="svg-rank">Megha Dandapat</p>
+                        </div>
+                        <div className="col">
+                        <img className="svgs" src ={gold} alt="My Happy SVG"/>
+                        <p className="svg-rank">Megha Dandapat</p>
+                        </div>
+                        <div className="col">
+                        <img className="svgs bronze svg-down" src ={bronze} alt="My Happy SVG"/>
+                        <p className="svg-rank">Megha Dandapat</p>
+                        </div>
+                    
+                    </div>
+
+                    {/* <div className="row">
+                    <img className="svgs" src ={silver} alt="My Happy SVG"/>2. Varada kalaskar</div>
+                    <div className="row"> 
+                    <img className="svgs" src ={bronze} alt="My Happy SVG"/>3. Esha Anvekar</div> */}
             </div>
             </div>
             </div>
