efcb039e301c6256ea4fd4c561781ee39f92941e
Varada Kalaskar <kalaskar.varada@gmail.com>
2021-05-18 16:24:43 +0530

    minor changes

--- src/components/QuestionHub/QuestionHub.js
+++ src/components/QuestionHub/QuestionHub.js
@@ -3,18 +3,26 @@ import { ProgressBar} from 'react-bootstrap';
 
 import Cards from "./Cards";
 import TitleCard from './TitleCard';
+import { useState } from 'react';
 
 
 const QuestionHub = () => {
-    const qs=[1,2,3,4,5,6];
+    const[qs,setQs] = useState([
+        {id:1, qsno:1, progress:90, attmp:3},
+        {id:2, qsno:2, progress:30, attmp:2},
+        {id:3, qsno:3, progress:10, attmp:1},
+        {id:4, qsno:4, progress:70, attmp:4},
+        {id:5, qsno:5, progress:50, attmp:9},
+        {id:6, qsno:6, progress:100, attmp:10}
+    ])
    
     return (  
         <div className="qhub">
             
             <TitleCard/>
             {qs.map((qs)=>(
-                <Cards qno={qs} progbar={<ProgressBar variant="secondary" now={Math.floor(Math.random() * (100 - 0))} className="progress" /> } 
-                attempts={Math.floor(Math.random() * 10) + 1} button="yes"/>
+                <Cards qno={qs.qsno} progbar={<ProgressBar variant="secondary" now={qs.progress} className="progress" /> } 
+                attempts={qs.attmp} button="yes"/>
             ))}
         </div>
     );
