025a345c345ac008fa476b67c354583964242aac
Varada Kalaskar <kalaskar.varada@gmail.com>
2021-06-21 18:23:56 +0530

    footer changed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import  './Footer.css';
 const Footer = () => {
     return (
         <Navbar fixed="bottom" expand="lg" variant="default" className="footer">
-          <Navbar.Brand>&copy; PICT IEEE Student Branch</Navbar.Brand>
+          <Navbar.Brand>&copy; PICT IEEE Student Branch OUR TEAM</Navbar.Brand>
         </Navbar>
        );
 }
