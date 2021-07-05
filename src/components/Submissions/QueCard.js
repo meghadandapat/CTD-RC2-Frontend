9cc582db56f3a5ce9dbaff64e665047e10bc4483
eshanvekar-01 <eshaanvekar@gmail.com>
2021-07-05 16:54:25 +0530

    question card changed

--- src/components/Submissions/QueCard.js
+++ src/components/Submissions/QueCard.js
@@ -1,25 +1,46 @@
-
+import { Table } from "react-bootstrap";
 const QueCard = ({qno}) => {
 
     return ( 
         <div className="QueCard">
-          <div className="row queCard">
-            <div className="col-sm-1"></div>
-              <div className="col-sm-10">
-                <div className="card qcard">
-                  <div className="card-body">
-                    <div className="row">
-                      <div className="col-sm-1"></div>
-                      <div className="col-sm-2 text-center">
-                        <p className="quetext">Question no {qno}</p>
-                      </div>
-                      <div className="col-sm-9"></div>
-                    </div>
-                  </div>
+            <div className="row">
+                <div className="col-sm-1">
+
                 </div>
-              </div>
-            <div className="col-sm-1"></div>
-        </div>
+
+                <div className="col-sm-10">
+                    <div className="question">
+                        <h3 className="title-text question-number">Question {qno}</h3>
+                    </div>                    
+                    <div className="card qcard">
+                        <div className="card-body mb-2">
+                            <Table responsive className="table table-borderless">
+                                <tbody>
+                                    <tr>
+                                        <td className="td1"></td>
+                                        <td className="srno justify-content-left">
+                                            <p className="title-text ">SrNo.</p>
+                                        </td>
+                                        <td className="stime">
+                                            <p className="title-text ">Time</p>
+                                        </td>
+                                        <td className="sprogress">
+                                            <p className="title-text ">Progress</p>
+                                        </td>
+                                        <td className="sview"></td>
+                                        
+                                        
+                                    </tr>
+                                </tbody>
+                            </Table>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-1">
+
+                </div>
+            </div>
        
         </div>
     );
