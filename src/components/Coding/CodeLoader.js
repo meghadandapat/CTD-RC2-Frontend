765141bfd34bd3f34a0a7cc483e87060b04f9c15
Megha <megha13dandapat@gmail.com>
2021-07-06 08:50:28 +0530

    bg static remaining

--- src/components/Coding/CodeLoader.js
+++ src/components/Coding/CodeLoader.js
@@ -1,12 +1,19 @@
 import './CodeLoader.css';
+import React from 'react'
 
 const CodeLoader = () => {
-    return ( <div class="spinner-box">
-    <div class="circle-border">
-      <div class="circle-core"></div>
-        </div>
-    <h6 style={{color:"#AB5FC6", textAlign:"center"}}>Your code is getting executed.</h6>    
-  </div> );
+    return (
+        <React.Fragment>
+            <div class="spinner-box">
+                <div class="circle-border">
+                    <div class="circle-core"></div>
+                </div>
+          
+            </div>
+            <h5 style={{ color: "#AB5FC6", textAlign: "center" }}>Your code is being executed...</h5>
+        </React.Fragment>
+    );
+        
 }
  
 export default CodeLoader;
