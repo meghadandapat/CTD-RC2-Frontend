d8561dac3a7fa1ec88587040bf355d25623d9d0b
Megha <megha13dandapat@gmail.com>
2021-07-21 01:06:28 +0530

    links added to webteam

--- src/components/WebTeam/OurTeam.js
+++ src/components/WebTeam/OurTeam.js
@@ -18,16 +18,16 @@ const OurTeam = () => {
       <Button onClick={goBack} className="close-button">
         X
       </Button>
-      <div className="container d-flex justify-content-center align-items-center" style={{ height: "80vh" }}>
+      <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: "80vh" }}>
         <div className="centered-team " style={{width:"100%"}}>
         <div className="row mar justify-content-center">
-          <WebCard name={"Varada Kalaskar"} img={Varada} github={"https://github.com/varada-kalaskar"} linkedin={""}/>
-          <WebCard name={"Megha Dandapat"} img={Megha} github={"https://github.com/meghadandapat"} linkedin={"" }/>
-          <WebCard name={"Esha Anvekar"} img={Esha} github={"https://github.com/eshanvekar-01" } linkedin={ ""}/>
+          <WebCard name={"Varada Kalaskar"} img={Varada} github={"https://github.com/varada-kalaskar"} linkedin={"https://www.linkedin.com/in/varada-kalaskar-2316b2217/"}/>
+            <WebCard name={"Megha Dandapat"} img={Megha} github={"https://github.com/meghadandapat"} linkedin={"https://www.linkedin.com/in/megha-dandapat-46152519b/"}/>
+          <WebCard name={"Esha Anvekar"} img={Esha} github={"https://github.com/eshanvekar-01" } linkedin={ "https://www.linkedin.com/in/esha-anvekar-998436211/"}/>
         </div>
         <div className="row mar justify-content-center" style={{marginTop:"2vw"}}>
-          <WebCard name={"Atharva Naphade"} img={Atharva} github={"https://github.com/atharvanaphade" } linkedin={"" }/>
-          <WebCard name={"Neil Deshpande"} img={Neil} github={"https://github.com/deshpandeneil" } linkedin={"" }/>
+            <WebCard name={"Atharva Naphade"} img={Atharva} github={"https://github.com/atharvanaphade"} linkedin={"https://www.linkedin.com/in/atharva-naphade-540236148/"} portfolio={"https://atharvanaphade.me" }/>
+          <WebCard name={"Neil Deshpande"} img={Neil} github={"https://github.com/deshpandeneil" } linkedin={"https://www.linkedin.com/in/neil-deshpande-908aa118b/" }/>
         </div>
         <div className="spaceteam"></div>
       </div>
--- src/components/WebTeam/WebCard.js
+++ src/components/WebTeam/WebCard.js
@@ -1,42 +1,41 @@
 import { Link } from "react-router-dom";
-import "./Team.css";
-const WebCard = ({ name, img, github, linkedin }) => {
-  return (
 
-    <div className="col-12 col-sm-6 col-md-4 col-lg-4 justify-content-center d-flex" >
-    <div className="our-team">
-      <div className="picture">
-        <img
-          className="img-fluid"
-            src={img}
-            alt={name}
-        />
-      </div>
-      <div className="team-content">
+import "./Team.css";
+const WebCard = ({ name, img, github, linkedin, portfolio, content }) => {
+  return (
+    <div className="col-12 col-sm-6 col-md-4 col-lg-4 justify-content-center d-flex">
+      <div className="our-team">
+      <a href={portfolio || github} target="_blank" style={{color:"#fff"}}>
+        <div className="picture">
+          <img className="img-fluid" src={img} alt={name} />
+          </div>
+        
+        <div className="team-content">
           <h4 className="name">{name}</h4>
-       
+            </div>
+            
+          </a>
+
+        <ul className="social">
+          <li>
+            <a
+              href={github}
+              className="fa fa-github"
+              aria-hidden="true"
+              target="_blank"
+            />
+          </li>
+          <li>
+            <a
+              href={linkedin}
+              className="fa fa-linkedin"
+              aria-hidden="true"
+              target="_blank"
+            />
+          </li>
+        </ul>
       </div>
-      <ul className="social">
-        <li>
-          <a
-            href={github}
-            className="fa fa-github"
-              aria-hidden="true"
-              target="_blank"
-          />
-        </li>
-        <li>
-          <a
-            href={linkedin}
-            className="fa fa-linkedin"
-              aria-hidden="true"
-              target="_blank"
-          />
-        </li>
-      </ul>
     </div>
-  </div>
-  
   );
 };
 
